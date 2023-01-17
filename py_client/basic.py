af8c4c8373ed2c025f05f45015b1cd9fbb3d8451
Gabriele Cardoso das Virgens <gabrielecardosov@gmail.com>
2023-01-17 19:27:47 -0300

    Django Model Instance to Dictionary

--- py_client/basic.py
+++ py_client/basic.py
@@ -4,9 +4,8 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://127.0.0.1:8000/api"
 
-get_response = requests.get(endpoint, params={"abc": 123}, json={"query":"Hello World"})
+get_response = requests.get(endpoint, json={"product_id": 123})
 # print(get_response.text)
+print(get_response.headers)
 print(get_response.status_code)
-
 print(get_response.json())
-# print(get_response.status_code)
