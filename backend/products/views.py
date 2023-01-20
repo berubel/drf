ebcd1c08da86f092d06e69a4fb98e58516d66da1
Gabriele Cardoso das Virgens <gabrielecardosov@gmail.com>
2023-01-20 19:04:29 -0300

    DRF Generics CreateAPIView

--- backend/products/views.py
+++ backend/products/views.py
@@ -5,6 +5,18 @@ from .serializers import ProductSerializer
 
 # Create your views here.
 
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        print(serializer.validated_data)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content')
+        if content is None:
+            content = title
+        serializer.save(content=content)
+
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
---
+++ py_client/create.py
@@ -0,0 +1,17 @@
+import requests
+
+# endpoint = "https://httpbin.org/status/200"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://127.0.0.1:8000/api/products/"
+
+data = {
+    "title": "Snacks",
+    "content":"Popcorn", 
+    "price": 4.50
+}
+
+get_response = requests.post(endpoint, json=data)
+# print(get_response.text)
+print(get_response.headers)
+print(get_response.status_code)
+print(get_response.json())
