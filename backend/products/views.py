6d6c09d77e110928fcfd1afd7a1506c11a2ba84a
Gabriele Cardoso das Virgens <gabrielecardosov@gmail.com>
2023-01-20 22:24:58 -0300

    Function Based Views for Create, Retrieve or List

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,7 +1,15 @@
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+#from django.http import Http404
+from django.shortcuts import get_object_or_404
 
 from .models import Product
 from .serializers import ProductSerializer
+
+
+
 
 # Create your views here.
 
@@ -28,3 +36,28 @@ Not gonna use this method
 class ProductListAPIView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+@api_view(['GET', 'POST'])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method
+
+    if method == "GET":
+        if pk is not None:
+            # detail view
+            obj = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj, many=False).data
+            return Response(data)
+
+        # list view
+        qs = Product.objects.all()
+        data = ProductSerializer(qs, many=True).data
+        return Response(data)
+
+    if method == "POST":
+        # create an item 
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            # instance = serializer.save()
+            print(serializer.data)
+            return Response(serializer.data)
+        return Response({'invalid':'Not good data'}, status=400)
