8399f9c4a4cf5af1aea175872342880e795486b8
Gabriele Cardoso das Virgens <gabrielecardosov@gmail.com>
2023-01-26 22:49:57 -0300

    Related Fields & Foreign Key Serializer

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,10 +1,22 @@
 from rest_framework import serializers
 from rest_framework.reverse import reverse
 
+from api.serializers import UserPublicSerializer
 from .models import Product
 from .validators import validate_title_no_hello, unique_product_title
 
+class ProductInlineSerializer(serializers.Serializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name='product-detail',
+        lookup_field='pk',
+        read_only=True
+    )
+    title = serializers.CharField(read_only=True)
+
 class ProductSerializer(serializers.ModelSerializer):
+    owner = UserPublicSerializer(source='user', read_only=True)
+    related_products = ProductInlineSerializer(source='user.product_set.all', read_only= True, many=True )
+    my_user_data = serializers.SerializerMethodField(read_only=True)
     my_discount = serializers.SerializerMethodField(read_only=True)
     edit_url = serializers.SerializerMethodField(read_only=True)
     url = serializers.HyperlinkedIdentityField(view_name='product-detail', lookup_field='pk')
@@ -15,7 +27,8 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = [
-            # 'user',
+            'owner',
+            'related_products',
             'url',
             'edit_url',
             # 'email',
@@ -25,7 +38,8 @@ class ProductSerializer(serializers.ModelSerializer):
             'content',
             'price',
             'sale_price',
-            'my_discount'
+            'my_discount',
+            'my_user_data'
         ]
     
     # def validate_title(self, value):
@@ -64,3 +78,8 @@ class ProductSerializer(serializers.ModelSerializer):
         if not isinstance(obj, Product):
             return None
         return obj.get_discount()
+    
+    def get_my_user_data(self, obj):
+        return {
+            'username': obj.user.username
+        }
